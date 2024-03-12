b6d40c0cf260ed0719fcf41621bd5d2d3bd00de7
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-12 16:17:58 +0000

    added partial testing

--- calculator.test.js
+++ calculator.test.js
@@ -16,26 +16,40 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    expected = 500000;
+    actual = sum(400000, 100000);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    expected = -50;
+    actual = sum(-24, -26);
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  test('can add zero', () => {
+    expected = 0;
+    actual = sum(0, 0);
+    expect(actual).toBe(expected);
   });
-
-});
 
 describe('subtract', () => {
-
+  test('can subtract two numbers', () => {
+  expected = 6;
+  actual = sum(10, 4);
+  expect(actual).toBe(expected);
+    }
+  )
 });
 
 describe('multiply', () => {
-
+  test('can multiply two numbers', () => {
+    expected = 6;
+    actual = sum(10, 4);
+    expect(actual).toBe(expected);
+      }
+    )
 });
 
 describe('divide', () => {
@@ -52,4 +66,5 @@ describe('even', () => {
 
 describe('odd', () => {
 
+});
 });
